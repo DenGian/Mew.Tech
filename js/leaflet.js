d8885faf4cc81153ce87045cffc3308ad24e556b
CedricDeMaesschalck <wakkawakka@hotmail.be>
2024-03-13 15:50:04 +0000

    Leaflet and login page and some minor changers

--- js/leaflet.js
+++ js/leaflet.js
@@ -1,8 +1,16 @@
 /* Leaflet */
+
 let map = L.map("map").setView([51.229835, 4.415417], 13);
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution:
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
-let marker = L.marker([51.229835, 4.415417]).addTo(map);
+let pokeball = L.icon({
+  iconUrl: "../../assets/images/poke-ball.png",
+  iconSize: [38, 95],
+  iconAnchor: [22, 94],
+  shadowAnchor: [4, 62],
+  popupAnchor: [-3, -76],
+});
+L.marker([51.229835, 4.41517], { icon: pokeball }).addTo(map);
