d9e2f3e55459bce761428a9c2de4762d5913bb1a
DenGian <mondelaers.ian@gmail.com>
2024-04-04 10:34:50 +0000

    started on client folder structure

---
+++ client/public/js/leaflet.js
@@ -0,0 +1,16 @@
+/* Leaflet */
+
+let map = L.map("map").setView([51.229835, 4.415417], 13);
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+let pokeball = L.icon({
+  iconUrl: "../../assets/images/poke-ball.png",
+  iconSize: [38, 45],
+  iconAnchor: [22, 20],
+  shadowAnchor: [4, 62],
+  popupAnchor: [-3, -76],
+});
+L.marker([51.229835, 4.41517], { icon: pokeball }).addTo(map);
