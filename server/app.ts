d6979e8c8ec7937eb27da584b83cf74fe5b9718e
DenGian <mondelaers.ian@gmail.com>
2024-04-04 10:35:08 +0000

    started on server structure

---
+++ server/app.ts
@@ -0,0 +1,28 @@
+import express, { Express } from "express";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config();
+
+const app : Express = express();
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "..", "client", "public")));
+app.set('views', path.join(__dirname, "..", "client", "views"));
+
+app.set("port", process.env.PORT || 3000);
+
+app.get("", (req, res) => {
+    res.type("text/html");
+    res.render("index");
+})
+
+app.use((req, res) => {
+    res.status(404).send("404, page not found");
+});
+
+app.listen(app.get("port"), () => {
+    console.log("Server started on http://localhost:" + app.get('port'));
+});
