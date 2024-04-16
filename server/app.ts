e7b868c93896d78f20f5fc77ad7bc981c47f9cb9
Jesus <150912607+Prometheus1993@users.noreply.github.com>
2024-04-16 14:17:39 +0000

    fetch

--- server/app.ts
+++ server/app.ts
@@ -12,21 +12,21 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "..", "client", "public")));
-app.set('views', path.join(__dirname, "..", "client", "views"));
+app.set("views", path.join(__dirname, "..", "client", "views"));
 
 app.set("port", process.env.PORT || 3000);
 
 app.get("", (req, res) => {
-    res.type("text/html");
-    res.render("index");
+  res.type("text/html");
+  res.render("index");
 });
 
 app.use("/", indexRouter);
 
 app.use((req, res) => {
-    res.status(404).send("404, page not found");
+  res.status(404).send("404, page not found");
 });
 
 app.listen(app.get("port"), () => {
-    console.log("Server started on http://localhost:" + app.get('port'));
+  console.log("Server started on http://localhost:" + app.get("port"));
 });
