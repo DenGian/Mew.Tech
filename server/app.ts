551e26cac0c82564997978d1908395f0ded97ad2
DenGian <mondelaers.ian@gmail.com>
2024-04-30 13:49:51 +0000

    add routes

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,16 @@ import express, { Express } from "express";
 import dotenv from "dotenv";
 import path from "path";
 import indexRouter from "./routes/index";
+import accesDeniedRouter from "./routes/accesDenied";
+import battlerRouter from "./routes/battler";
+import catcherRouter from "./routes/catcher";
+import compareRouter from "./routes/compare";
+import contactRouter from "./routes/contact";
+import guessRouter from "./routes/guess";
+import loginRouter from "./routes/login";
+import pokeDexRouter from "./routes/pokeDex";
+import registerRouter from "./routes/register";
+import viewerRouter from "./routes/viewer";
 import { handleError } from "./middleware/handleError";
 import { loggingMiddleware } from "./middleware/handleLogging";
 import { pageNotFoundMiddleware } from "./middleware/handlePageNotFound";
@@ -22,6 +32,17 @@ app.set("port", process.env.PORT || 3000);
 app.use(loggingMiddleware);
 
 app.use("/", indexRouter);
+app.use("/acces-denied", accesDeniedRouter);
+app.use("/battler", battlerRouter);
+app.use("/catcher", catcherRouter);
+app.use("/compare", compareRouter);
+app.use("/contact", contactRouter);
+app.use("/guess", guessRouter);
+app.use("/login", loginRouter);
+app.use("/pokeDex", pokeDexRouter);
+app.use("/register", registerRouter);
+app.use("/viewer", viewerRouter);
+
 
 app.use(handleError);
 
