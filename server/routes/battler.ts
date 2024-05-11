0227c12615af2ce128f869ec9cc44ce0dec8b8e0
DenGian <mondelaers.ian@gmail.com>
2024-05-11 13:24:00 +0000

    rm import

--- server/routes/battler.ts
+++ server/routes/battler.ts
@@ -1,16 +1,16 @@
 import express, { Router } from "express";
-import { fetchPokemonData } from "../utils/helper-functions";
+
 
 const router: Router = express.Router();
 
-router.get("/", async (req, res) => {
-    try {
-      const pokemonList = await fetchPokemonData(2);
-      res.render("pokeBattler", { pokemonList });
-    } catch (error) {
-      console.error("Error fetching Pokémon data:", error);
-      res.status(500).send("Failed to load page due to server error.");
-    }
-  });
+// router.get("/", async (req, res) => {
+//     try {
+//       const pokemonList = await fetchPokemonData(2);
+//       res.render("pokeBattler", { pokemonList });
+//     } catch (error) {
+//       console.error("Error fetching Pokémon data:", error);
+//       res.status(500).send("Failed to load page due to server error.");
+//     }
+//   });
 
 export default router;
