bb2ffe44a71f3e73622f74a350d7d1b7d4cb93a3
DenGian <mondelaers.ian@gmail.com>
2024-05-26 19:51:35 +0000

    updated functions for new db functionalities

--- server/utils/helper-functions.ts
+++ server/utils/helper-functions.ts
@@ -1,29 +1,36 @@
+// helper-functions.ts
+
 import { getAllPokemon, getPokemonById } from "../config/database";
+import { PokemonData } from "../interfaces/pokemonInterface";
 
 // Function to display a random Pokémon
-
-async function displayRandomPokemon() {
-  try {
-    const allPokemon = await getAllPokemon(); // Retrieve all Pokémon from the database
-    if (allPokemon.length === 0) {
-      throw new Error("No Pokémon found in the database.");
+async function displayRandomPokemon(): Promise<PokemonData | null> {
+    try {
+        const { pokemonData } = await getAllPokemon(0, 0); // Retrieve all Pokémon from the database
+        if (pokemonData.length === 0) {
+            throw new Error("No Pokémon found in the database.");
+        }
+        const randomIndex = Math.floor(Math.random() * pokemonData.length); // Select a random index
+        return pokemonData[randomIndex]; // Return the randomly selected Pokémon
+    } catch (error) {
+        console.error("Failed to fetch random Pokémon:", error);
+        return null;
     }
-    const randomIndex = Math.floor(Math.random() * allPokemon.length); // Select a random index
-    return allPokemon[randomIndex]; // Return the randomly selected Pokémon
-  } catch (error) {
-    console.error("Failed to fetch random Pokémon:", error);
-    return null;
-  }
 }
+
 // Function to catch a Pokémon
 function catchPokemon() {
-  const catchRate = 0.25; // Chance to catch the Pokémon
-  const random = Math.random(); // Generate a random number between 0 and 1
-  if (random < catchRate) {
-    return { message: "You caught the Pokémon!", caught: true };
-  } else {
-    return { message: "The Pokémon escaped!", caught: false };
-  }
+    const catchRate = 0.25; // Chance to catch the Pokémon
+    const random = Math.random(); // Generate a random number between 0 and 1
+    if (random < catchRate) {
+        return { message: "You caught the Pokémon!", caught: true };
+    } else {
+        return { message: "The Pokémon escaped!", caught: false };
+    }
 }
 
-export { displayRandomPokemon, catchPokemon };
+function calculateTotalPages(totalPokemonCount: number, pokemonPerPage: number): number {
+    return Math.ceil(totalPokemonCount / pokemonPerPage);
+}
+
+export { displayRandomPokemon, catchPokemon, calculateTotalPages };
