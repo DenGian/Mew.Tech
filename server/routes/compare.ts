b4c0725b08c856c828f0cd62833b969148f27efc
Jesus <150912607+Prometheus1993@users.noreply.github.com>
2024-05-27 23:01:17 +0000

    changes to ejs

--- server/routes/compare.ts
+++ server/routes/compare.ts
@@ -1,36 +1,61 @@
 import express, { Router } from "express";
-import { displayRandomPokemon } from "../utils/helper-functions";
+import {
+  displayRandomPokemon,
+  capitalizeFirstLetter,
+} from "../utils/helper-functions";
 import { filteredPokemon } from "../config/database";
 
 const router: Router = express.Router();
 
 router.get("/", async (req, res) => {
-    try {
-        const searchPokemon1: string | undefined = req.query.searchPokemon1 as string | undefined;
-        const searchPokemon2: string | undefined = req.query.searchPokemon2 as string | undefined;
+  try {
+    const searchPokemon1: string | undefined = req.query.searchPokemon1 as
+      | string
+      | undefined;
+    const searchPokemon2: string | undefined = req.query.searchPokemon2 as
+      | string
+      | undefined;
 
-        const randomPokemonResult = searchPokemon1 ? await filteredPokemon(searchPokemon1) : await displayRandomPokemon();
-        const randomPokemon2Result = searchPokemon2 ? await filteredPokemon(searchPokemon2) : await displayRandomPokemon();
+    const randomPokemonResult = searchPokemon1
+      ? await filteredPokemon(searchPokemon1)
+      : await displayRandomPokemon();
+    const randomPokemon2Result = searchPokemon2
+      ? await filteredPokemon(searchPokemon2)
+      : await displayRandomPokemon();
 
-        const randomPokemon = Array.isArray(randomPokemonResult) ? randomPokemonResult[0] : randomPokemonResult;
-        const randomPokemon2 = Array.isArray(randomPokemon2Result) ? randomPokemon2Result[0] : randomPokemon2Result;
+    const randomPokemon = Array.isArray(randomPokemonResult)
+      ? randomPokemonResult[0]
+      : randomPokemonResult;
+    const randomPokemon2 = Array.isArray(randomPokemon2Result)
+      ? randomPokemon2Result[0]
+      : randomPokemon2Result;
 
-        const stats1 = randomPokemon ? randomPokemon.stats.map(stat => stat.base_stat) : [];
-        const stats2 = randomPokemon2 ? randomPokemon2.stats.map(stat => stat.base_stat) : [];
-
-        res.render("pokeCompare", 
-        { 
-          randomPokemon, 
-          randomPokemon2, 
-          searchPokemon1, 
-          searchPokemon2, 
-          stats1, 
-          stats2 
-        });
-    } catch (error) {
-        console.error("Error fetching Pokémon:", error);
-        res.status(500).send("Failed to load page due to server error.");
+    if (randomPokemon) {
+      randomPokemon.name = capitalizeFirstLetter(randomPokemon.name);
     }
+    if (randomPokemon2) {
+      randomPokemon2.name = capitalizeFirstLetter(randomPokemon2.name);
+    }
+
+    const stats1 = randomPokemon
+      ? randomPokemon.stats.map((stat) => stat.base_stat)
+      : [];
+    const stats2 = randomPokemon2
+      ? randomPokemon2.stats.map((stat) => stat.base_stat)
+      : [];
+
+    res.render("pokeCompare", {
+      randomPokemon,
+      randomPokemon2,
+      searchPokemon1,
+      searchPokemon2,
+      stats1,
+      stats2,
+    });
+  } catch (error) {
+    console.error("Error fetching Pokémon:", error);
+    res.status(500).send("Failed to load page due to server error.");
+  }
 });
 
 export default router;
